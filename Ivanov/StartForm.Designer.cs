f209c46c4a81886e4aa599d53d3f7830a1e5ef7a
iljuha222@gmail.com <iljuha222@gmail.com>
2021-12-28 23:18:03 +0200

    MaksimStudzinskiy update

--- Ivanov/StartForm.Designer.cs
+++ Ivanov/StartForm.Designer.cs
@@ -29,40 +29,47 @@ namespace Ivanov
         /// </summary>
         private void InitializeComponent()
         {
-            this.start = new System.Windows.Forms.Button();
-            this.newBS = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // start
-            // 
-            this.start.Location = new System.Drawing.Point(157, 156);
-            this.start.Name = "start";
-            this.start.Size = new System.Drawing.Size(201, 53);
-            this.start.TabIndex = 0;
-            this.start.Text = "Start";
-            this.start.UseVisualStyleBackColor = true;
-            this.start.Click += new System.EventHandler(this.start_Click);
-            // 
-            // newBS
-            // 
-            this.newBS.Location = new System.Drawing.Point(157, 60);
-            this.newBS.Name = "newBS";
-            this.newBS.Size = new System.Drawing.Size(201, 53);
-            this.newBS.TabIndex = 1;
-            this.newBS.Text = "New";
-            this.newBS.UseVisualStyleBackColor = true;
-            this.newBS.Click += new System.EventHandler(this.newBS_Click);
-            // 
-            // StartForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 309);
-            this.Controls.Add(this.newBS);
-            this.Controls.Add(this.start);
-            this.Name = "StartForm";
-            this.Text = "StartForm";
-            this.ResumeLayout(false);
+			this.start = new System.Windows.Forms.Button();
+			this.newBS = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// start
+			// 
+			this.start.Font = new System.Drawing.Font("Minion Pro", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.start.Location = new System.Drawing.Point(13, 204);
+			this.start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.start.Name = "start";
+			this.start.Size = new System.Drawing.Size(658, 163);
+			this.start.TabIndex = 0;
+			this.start.Text = "Start";
+			this.start.UseVisualStyleBackColor = true;
+			this.start.Click += new System.EventHandler(this.start_Click);
+			// 
+			// newBS
+			// 
+			this.newBS.Font = new System.Drawing.Font("Minion Pro", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.newBS.Location = new System.Drawing.Point(13, 13);
+			this.newBS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.newBS.Name = "newBS";
+			this.newBS.Size = new System.Drawing.Size(658, 166);
+			this.newBS.TabIndex = 1;
+			this.newBS.Text = "New";
+			this.newBS.UseVisualStyleBackColor = true;
+			this.newBS.Click += new System.EventHandler(this.newBS_Click);
+			// 
+			// StartForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.ClientSize = new System.Drawing.Size(684, 380);
+			this.Controls.Add(this.newBS);
+			this.Controls.Add(this.start);
+			this.ForeColor = System.Drawing.SystemColors.MenuText;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "StartForm";
+			this.Text = "Block-Schemer";
+			this.ResumeLayout(false);
 
         }
 
